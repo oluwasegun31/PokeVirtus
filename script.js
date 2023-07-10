a15495032d32ff65dbe277ac7a2f4c30a794ab6e
oluwasegun31 <oluwasegun1412@gmail.com>
2023-07-10 16:56:39 +0100

    Random Number

--- script.js
+++ script.js
@@ -9,14 +9,23 @@ const landSpeed = document.querySelector('#landSpeed');
 
 async function pokemonOfTheDay(){
     try{
-        let response = await fetch('https://pokeapi.co/api/v2/pokemon/800');
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/10`);
+        // to throw an error if response is false
         if(!response.ok){
             throw new Error(`${response.status}: Unable to Load`);
         }
         let data = await response.json();
+        // pokemon of the day name
         landingName.textContent = data.name;
+        //pokemon of the day type
         landingType.textContent = data.types[0].type.name;
-        landingImg.src = data.sprites.other["official-artwork"].front_shiny;
+        // image for pokemon of the day
+        if(data.sprites.other["official-artwork"].front_shiny === null){
+            landingImg.src = data.sprites.other["official-artwork"].front_default;
+        }else{
+            landingImg.src = data.sprites.other["official-artwork"].front_shiny;
+        }
+        // stats for pokemon of the day
         landHp.textContent = data.stats[0].base_stat;
         landAttack.textContent = data.stats[1].base_stat;
         landDefense.textContent = data.stats[2].base_stat;
